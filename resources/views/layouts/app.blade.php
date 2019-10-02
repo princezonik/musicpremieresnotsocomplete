090d9d121467204615766801cbbc544ef139a79f
Prince Efik <princezonik@gmail.com>
2019-10-02 14:43:53 +0100

    all files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
+
+</head>
+    <body>
+    
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
